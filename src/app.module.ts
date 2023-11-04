f17e71cdfe8ba8478bb91cc06637bfd984bceae3
Oscar Buitrago <o.buitragov@uniandes.edu.co>
2023-11-04 14:19:32 -0500

    Ajuste Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,9 +47,6 @@ import { AlbumMusicianModule } from './albummusician/albummusician.module';
       keepConnectionAlive: true,
       migrations: [__dirname + '/migration/**/*{.ts,.js}'],
       migrationsRun: true,
-      extra: {
-        ssl: true
-      },
       ssl: {
         rejectUnauthorized: false
       }
